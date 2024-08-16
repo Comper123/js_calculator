d65fdd08233ddb3d4a16eafe679f31b43e86eda2
Comper123 <92387091+Comper123@users.noreply.github.com>
2024-08-17 02:02:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const actions = ['/', '%', '*', '-', '+', '.'];
+
 function clr() {
     document.getElementById('calc').value = "";
     document.getElementById('last_calc').value = "";
@@ -5,24 +7,43 @@ function clr() {
 
 function clear_elem() {
     var calc = document.getElementById('calc');
-    calc.value = calc.value.slice(0, -1);
+    if (calc.value == "Error") {
+        calc.value = "";
+    } else {
+        calc.value = calc.value.slice(0, -1);
+    }
 }
 
 function add(elem) {
     var calc = document.getElementById('calc');
-    if (calc.value == "Error") {
-        calc.value = ""
+    if (calc.value == "Error" || (calc.value == "" && actions.includes(elem) && elem !== "-")) {
+        calc.value = "";
+        return
     }
-    calc.value += elem;
+    if (actions.includes(elem) && actions.includes(calc.value.slice(-1))){
+        if (elem == calc.value.slice(-1)){
+            calc.value += "";
+        } else {
+            calc.value = calc.value.slice(0, -1)
+            calc.value += elem
+        }
+    } else {
+        calc.value += elem;
+    }
 }
 
 function calc() {
     var calc = document.getElementById('calc');
+    if (actions.includes(calc.value.slice(-1)) || calc.value == ""){
+        return
+    }
     var last_calc = document.getElementById('last_calc');
-    last_calc.value = calc.value;
-    try {
-        calc.value = eval(calc.value);
-    } catch {
-        calc.value = "Error";
+    if (calc.value !== 'Error'){
+        last_calc.value = calc.value;
+        try {
+            calc.value = eval(calc.value);
+        } catch {
+            calc.value = "Error";
+        }
     }
 }
